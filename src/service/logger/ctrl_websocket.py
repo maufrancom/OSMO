963a30ffe9d2a1287230c9e58fab1fc9502232e5
osmo-agent <osmo-agent@nvidia.com>
2026-03-26 04:17:38 +0000

    agent(pydantic-v2): Fix typo or minor change in logger websocket controller

--- src/service/logger/ctrl_websocket.py
+++ src/service/logger/ctrl_websocket.py
@@ -59,7 +59,7 @@ def update_metrics(
     ):
     """ Updates the metrics with the given workflow and group_name in the database. """
     database = connectors.PostgresConnector.get_instance()
-    metrics = getattr(metrics_options, metrics_options.__fields_set__.pop())
+    metrics = getattr(metrics_options, metrics_options.model_fields_set.pop())
     if isinstance(metrics, task.TaskGroupMetrics):
         task.TaskGroup.patch_metrics_in_db(
             database=database,
